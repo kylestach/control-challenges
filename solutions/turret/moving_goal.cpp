94b2931cb8fccc566caac071dbf9c71e97d94d69
Finn O'Toole Boire <finnegan.mccool@gmail.com>
2016-06-19 14:42:32 +0200

    Moving goal challenge for turret

---
+++ solutions/turret/moving_goal.cpp
@@ -0,0 +1,10 @@
+#include "moving_goal.h"
+#include <iostream>
+
+// Return a voltage to move the turret to a non-static goal
+Voltage TurretController::Update(TurretState state, Time dt) {
+  std::cout << "Implement the control function here! "
+               "(solutions/turret/moving_goal.cpp)"
+            << std::endl;
+  return 0 * V; 
+}
