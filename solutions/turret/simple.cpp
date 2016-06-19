208cbf1132e4f0343ce34573babe46c769a5a376
Finn O'Toole Boire <finnegan.mccool@gmail.com>
2016-06-19 14:41:24 +0200

    Simple turret challenge

---
+++ solutions/turret/simple.cpp
@@ -0,0 +1,10 @@
+#include "simple.h"
+#include <iostream>
+
+// Return a force to move the turret to 70 degrees
+Voltage TurretController::Update(TurretState state, Time dt) {
+  std::cout << "Implement the control function here! "
+               "(solutions/turret/simple.cpp)"
+            << std::endl;
+  return 0 * V; 
+}
